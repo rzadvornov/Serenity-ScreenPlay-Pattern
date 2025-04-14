dedc30c343806b53740b2dcc97b2349538c12305
rzadvornov <57909766+rzadvornov@users.noreply.github.com>
2025-04-14 13:27:44 +0300

    Added EnvConfigs

---
+++ EnvConfigs.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  * Copyright (c) Roman Zadvornov
+ *  * Unauthorized copying of this file, via any medium is strictly prohibited
+ *  * Proprietary and confidential
+ *
+ *
+ */
+
+package org.softindustry.com.config;
+
+
+import org.softindustry.com.utilities.JsonParser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class EnvConfigs {
+
+    private EnvConfigs() {
+    }
+
+    private static final Map<String, Object> ENV_CONFIG = new HashMap<>();
+
+    public static void setConfigs() {
+        ENV_CONFIG.putAll(Objects.requireNonNull(JsonParser.parseConfigFile()));
+    }
+
+    public static Object getConfig(final String key) {
+        return ENV_CONFIG.get(key);
+    }
+}
